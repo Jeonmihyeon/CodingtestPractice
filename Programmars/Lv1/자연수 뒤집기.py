f3dc059a89ff16c7f7a414d34625cde2c4c38d9e
Jeonmihyeon <50192595+Jeonmihyeon@users.noreply.github.com>
2022-11-08 22:55:38 +0900

    Create 자연수 뒤집기.py

---
+++ Programmars/Lv1/자연수 뒤집기.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12932
+def solution(n):
+    answer = []
+    
+    n_str = str(n)
+    
+    for i in range(1,len(n_str)):
+        answer.append(int(n_str[len(n_str) - i]))
+    
+    answer.append(int(n_str[0]))
+    
+    return answer
