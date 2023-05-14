cf27025b1e323e55d8fd7ee1124f314448618a47
Jeonmihyeon <50192595+Jeonmihyeon@users.noreply.github.com>
2023-05-15 02:24:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,13 +1,3 @@
 def solution(s):
-    alpha = "0123456789"
-    count = 0
-    if len(s) == 4 or len(s) == 6:
-        for i in range(0,len(s)):
-            if s[i] in alpha:
-                count += 1
-                
-    if count == len(s):
-        return True
-    else:
-        return False
-    
+    answer = True
+    return answer
